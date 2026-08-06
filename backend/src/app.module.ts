d467f35614de8ab6889dfad20d6fdaa3a30f70cf
Mohsin <mohsinnaveed196@gmail.com>
2026-08-06 16:54:19 +0500

    add environment-based database configuration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,29 +2,35 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
-import{TypeOrmModule} from "@nestjs/typeorm"
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './modules/user/user.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import jwtConfig from './modules/auth/config/jwt.config';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './modules/auth/guards/jwt-auth/jwt-auth.guard';
 
 @Module({
-  imports: [AuthModule,TypeOrmModule.forRoot({
-    type:"postgres",
-    port:5432,
-    host:"localhost",
-    username:"postgres",
-    password:"admin",
-    database:"shine_noteapp",
-    entities:[__dirname,"/**/*.entity{.ts,.js}"],
-    synchronize:true,
-    autoLoadEntities:true,
-
-  }), UserModule ,ConfigModule.forRoot({isGlobal:true,load:[jwtConfig]})],
-  controllers: [AppController],
-  providers: [AppService,
-    {provide:APP_GUARD,useClass:JwtAuthGuard}
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [jwtConfig] }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: "postgres",
+        port: config.get<number>("DB_HOST"),
+        host: config.get<string>("DB_HOST"),
+        username: config.get<string>("DB_USERNAME"),
+        password: config.get<string>("DB_PASSWORD"),
+        database: config.get<string>("DB_DATABASE"),
+        entities: [__dirname, '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+    
+      }),
+    }),
+    AuthModule,
+    UserModule,
+    ConfigModule.forRoot({ isGlobal: true, load: [jwtConfig] }),
   ],
+  controllers: [AppController],
+  providers: [AppService, { provide: APP_GUARD, useClass: JwtAuthGuard }],
 })
 export class AppModule {}
